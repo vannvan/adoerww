5c186ddb6a6a2b802e668f068e323f677d94aac8
vannvan <862670198@qq.com>
2020-01-21 11:43:09 +0800

    auto-commit

--- nav/data.js
+++ nav/data.js
@@ -2,7 +2,7 @@
 * @Author: vannvan <https://github.com/vannvan>
 * @Date:   2019-08-22 14:26:15
 * @Last Modified by:   vannvan
-* @Last Modified time: 2020-01-21 11:34:46
+* @Last Modified time: 2020-01-21 11:42:26
 */
 var msg= "万头攒动火树银花之处不必找我。如欲相见，我在各种悲喜交集处，能做的只是长途跋涉的归真返璞-by木心";
 var timer = null
@@ -172,6 +172,17 @@ $(document).ready(function(){
       }
       $("#"+elName).append(htmlStr)
     }
+    (function() {
+      let count = 0
+      WEBSITE.map(el => {
+        el.linkList.map(subEl => {
+          count ++ 
+        })
+      })
+      console.log(count)
+      let htmlStr = '已收录' + count + '个'
+      $("#included").append(htmlStr)
+    })();
 
     function RandomColor() {
        let r, g, b;
