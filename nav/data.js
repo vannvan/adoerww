deddf293ed5a8b5afcd165d25132ef0aa8a95d08
vannvan <862670198@qq.com>
2020-10-10 19:44:21 +0800

    auto-commit

--- nav/data.js
+++ nav/data.js
@@ -309,6 +309,9 @@ $(document).ready(function() {
         }, {
             name: '在线生成字符图案',
             link: 'http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20'
+        }, {
+            name: Ï "壁纸",
+            link: "https://wallhere.com/"
         }]
     }, {
         'name': '世间百态',
