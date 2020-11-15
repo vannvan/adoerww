08a82fbf4f5d7a739031baa832fb99f70988e6b7
vannvan <862670198@qq.com>
2020-11-15 19:51:23 +0800

    auto-commit

--- create-qrcode-chrome-extension/js/content-script.js
+++ create-qrcode-chrome-extension/js/content-script.js
@@ -1,8 +1,8 @@
 ﻿//接收消息
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request, '来自popup的消息');
-    console.log(sender.tab ?
-        "from a content script:" + sender.tab.url :
-        "from the extension");
-    sendResponse({ farewell: "goodbye" });
-});
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     console.log(request, '来自popup的消息');
+//     console.log(sender.tab ?
+//         "from a content script:" + sender.tab.url :
+//         "from the extension");
+//     sendResponse({ farewell: "goodbye" });
+// });
