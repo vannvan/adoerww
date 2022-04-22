1d57b2983e7f3a98899ee0ec46cd806c15f21ed1
vannvan <862670198@qq.com>
2022-04-22 09:53:48 +0800

    auto-commit

---
+++ react-jest/react-components-test/src/Button.js
@@ -0,0 +1,11 @@
+// Button.js
+import React from 'react'
+
+const Button = ({ increment, onClickFunction }) => {
+  const handleClick = () => {
+    onClickFunction(increment)
+  }
+  return <button onClick={handleClick}>+{increment}</button>
+}
+
+export default Button
