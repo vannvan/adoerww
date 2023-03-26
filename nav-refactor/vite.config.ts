060c519610c7b8b3f7da28d18b1938012194b3b3
vannvan <862670198@qq.com>
2023-03-26 18:10:20 +0800

    auto-commit

--- nav-refactor/vite.config.ts
+++ nav-refactor/vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { inject, minify } from 'vite-plugin-parse-html'
 
-console.log('process', process.env.NODE_ENV)
+// console.log('process', process.env.NODE_ENV)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    inject({
+      data: {
+        title: '导了个航',
+        description:
+          '导了个航是一个面向前端开发者的专属导航网站，提供便捷的导航功能，便于在工作中查找各类工具网站地址',
+        keywords: '前端导航、前端常用网站、前端工具、前端博客',
+      },
+    }),
+    minify({
+      isMinify: true,
+    }),
+  ],
   // publicDir: process.env.NODE_ENV == 'prod' ? './' : './public/',
   base: './',
   // assetsInclude: ['**/*.css'],
--- nav-refactor/vite.config.ts.js
+++ nav-refactor/vite.config.ts.js
@@ -1,18 +1,17 @@
 // vite.config.ts
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
-console.log("process", process.env.NODE_ENV);
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+console.log('process', process.env.NODE_ENV)
 var vite_config_default = defineConfig({
   plugins: [react()],
-  base: "./",
+  base: './',
+
   css: {
     preprocessorOptions: {
       less: {
-        charset: false
-      }
-    }
-  }
-});
-export {
-  vite_config_default as default
-};
+        charset: false,
+      },
+    },
+  },
+})
+export { vite_config_default as default }
