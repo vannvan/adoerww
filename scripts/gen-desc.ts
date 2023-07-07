79e8a4e7a0e365943051cc194813d2f8fa1dca48
vannvan <862670198@qq.com>
2023-07-07 20:37:27 +0800

    2023-07-07 20:37:22

--- scripts/gen-desc.ts
+++ scripts/gen-desc.ts
@@ -4,7 +4,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-06-12 16:20:44
+ * Last Modified: 2023-07-07 20:37:16
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -23,7 +23,7 @@ console.log('hello world')
     if (stat.isDirectory() && !exclude.includes(item)) {
       const isExit = await F.isExit(readmeFile)
       if (!isExit) {
-        const content = `# ${item} \n ## 描述`
+        const content = `# ${item} \n ## 描述 \n ## 创建时间 \n ${Date}`
         F.touch2(readmeFile, content)
         Log.success(`${readmeFile}文件已创建`)
       }
